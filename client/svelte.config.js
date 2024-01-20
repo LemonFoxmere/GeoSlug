f66b26f2c1dd43b50a1a2a5f5ca9fba680ac0458
Lemon Foxmere <lemon@lemonfoxmere.com>
2024-01-19 23:29:38 -0800

    fixed absolute path with sveltekit

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,11 +1,33 @@
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { fileURLToPath } from "node:url";
+import path from "path";
 
-/** @type {import("@sveltejs/kit").Config} */
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({
+		style: {
+			css: {
+				preprocessorOptions: {
+					scss: {
+						importer: [
+							(url) => {
+								if (url.startsWith("$static")) {
+									return {
+										file: url.replace(/^\$static/, path.join(dirname, "static"))
+									};
+								}
+								return url;
+							}
+						]
+					}
+				}
+			}
+		}
+	}),
 
 	onwarn: (warning, handler) => {
 		if (warning.code === "css-unused-selector" || warning.code.startsWith("a11y-")) {
@@ -21,7 +43,7 @@ const config = {
 		adapter: adapter(),
 
 		alias: {
-			$static: "./static",
+			$static: "static",
 			$comp: "$lib/comp",
 			$route: "src/routes"
 		}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,5 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -6,5 +7,10 @@ export default defineConfig({
 	server: {
 		port: 3001,
 		host: "0.0.0.0"
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve("src") // Styles in src/styles will be accessible as '@/styles/whatever.scss'
+		}
 	}
 });
